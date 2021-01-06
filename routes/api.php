f41e9d73483e0cdf298c27b65a4ca121ab0df666
Jhony Cacuango <jhony.cacuango@epn.edu.ec>
2021-01-05 23:50:14 -0500

    #16 Creacion de rutas #19 Creacion de controlador, se crea la ruta y el controlador de categoria

--- routes/api.php
+++ routes/api.php
@@ -16,9 +16,19 @@ use Illuminate\Http\Request;
 /*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });*/
+
+
 Route::post('register', 'UserController@register');
 Route::post('login', 'UserController@authenticate');
 Route::get('articles', 'ArticleController@index');
+
+
+Route::get('comentaries', 'ComentaryController@index');
+Route::get('comentaries/{comentary}', 'ComentaryController@show');
+Route::post('comentaries', 'ComentaryController@store');
+Route::put('comentaries/{comentary}', 'ComentaryController@update');
+Route::delete('comentaries/{comentary}', 'ComentaryController@delete');
+
 Route::group(['middleware' => ['jwt.verify']], function() {
 
     Route::get('user', 'UserController@getAuthenticatedUser');
@@ -26,4 +36,10 @@ Route::group(['middleware' => ['jwt.verify']], function() {
     Route::post('articles', 'ArticleController@store');
     Route::put('articles/{article}', 'ArticleController@update');
     Route::delete('articles/{article}', 'ArticleController@delete');
+
+
+
+
 });
+
+
